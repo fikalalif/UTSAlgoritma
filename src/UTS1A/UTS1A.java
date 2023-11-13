2ec0188511f6bcf4b4ad08230a3ea08070fc8e96
fikalalif <fikalgans26@gmail.com>
2023-11-13 15:34:41 +0700

    UTS

--- src/UTS1A/UTS1A.java
+++ src/UTS1A/UTS1A.java
@@ -1,2 +1,40 @@
-package UTS1A;public class UTS1A {
+package UTS1A;
+
+import java.util.Scanner;
+public class UTS1A {
+    public static void main(String[] args) {
+
+        String kodeBarang;
+        String namaBarang;
+        int qty;
+        double harga;
+        double totalHarga;
+
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Masukan kode barang: ");
+        kodeBarang = input.nextLine();
+
+        System.out.print("Masukan nama barang: ");
+        namaBarang = input.nextLine();
+
+        System.out.print("Masukan qty barang: ");
+        qty = input.nextInt();
+
+        System.out.print("Masukan harga barang: ");
+        harga = input.nextInt();
+
+
+        totalHarga = qty * harga;
+        System.out.println();
+
+        System.out.println("Kode barang: " + kodeBarang);
+        System.out.println("Nama barang: " + namaBarang);
+        System.out.println("Quality: " + qty);
+        System.out.println("Harga: " + harga);
+        System.out.println("Total harga: " + totalHarga);
+
+    }
 }
